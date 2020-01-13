74b0c2e7d16e22c2e33ce93e08eb9a5209682538
Julien Lin <julien.lin@ensta-paris.fr>
2020-01-13 23:50:56 +0100

    add timer for each big step

--- TravauxPratiques/Projet/Projet_land_ants/project/ant_simu.cpp
+++ TravauxPratiques/Projet/Projet_land_ants/project/ant_simu.cpp
@@ -13,19 +13,35 @@
 # include "gui/quad.hpp"
 # include "gui/event_manager.hpp"
 # include "display.hpp"
+#include "utils.hpp"
+
+
+using ratio= std::ratio<1, 1000>;
 
 void advance_time( const fractal_land& land, pheromone& phen, 
                    const position_t& pos_nest, const position_t& pos_food,
-                   std::vector<ant>& ants, std::size_t& cpteur )
+                   std::vector<ant>& ants, std::size_t& cpteur, std::chrono::time_point<std::chrono::high_resolution_clock>&timer, bool &is_first_time )
 {
-    for ( size_t i = 0; i < ants.size(); ++i )
+    for ( size_t i = 0; i < ants.size(); ++i ){
+
         ants[i].advance(phen, land, pos_food, pos_nest, cpteur);
+        if(cpteur == 1 && is_first_time){
+            timer = std::chrono::high_resolution_clock::now();
+            is_first_time = false;
+        }
+    }
     phen.do_evaporation();
     phen.update();
 }
 
 int main(int nargs, char* argv[])
 {
+
+    std::chrono::duration<double,ratio> init_fractal_time;
+    std::chrono::duration<double,ratio> init_ant_time;
+    std::chrono::time_point<std::chrono::high_resolution_clock> first_food_time;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_finding;
+    bool is_first_time  = true;
     const int nb_ants = 5000; // Nombre de fourmis
     const double eps = 0.8;  // Coefficient d'exploration
     const double alpha=0.7; // Coefficient de chaos
@@ -36,6 +52,8 @@ int main(int nargs, char* argv[])
     //const int i_nest = 256, j_nest = 256;
     // Location de la nourriture
     position_t pos_food{500,500};
+
+    auto start = std::chrono::high_resolution_clock::now(); // Compute init fractal land time
     //const int i_food = 500, j_food = 500;    
     // Génération du territoire 512 x 512 ( 2*(2^8) par direction )
     fractal_land land(8,2,1.,1024);
@@ -53,8 +71,14 @@ int main(int nargs, char* argv[])
         for ( fractal_land::dim_t j = 0; j < land.dimensions(); ++j )  {
             land(i,j) = (land(i,j)-min_val)/delta;
         }
+
+    auto end= std::chrono::high_resolution_clock::now(); // Compute init fractal land time
+    init_fractal_time = end - start;
+
     // Définition du coefficient d'exploration de toutes les fourmis.
     ant::set_exploration_coef(eps);
+    start = std::chrono::high_resolution_clock::now(); // Compute init ant time
+
     // On va créer des fourmis un peu partout sur la carte :
     std::vector<ant> ants;
     ants.reserve(nb_ants);
@@ -63,6 +87,9 @@ int main(int nargs, char* argv[])
     std::uniform_int_distribution<size_t>    ant_pos( 0, land.dimensions()-1 );
     for ( size_t i = 0; i < nb_ants; ++i )
         ants.push_back({{ant_pos(gen),ant_pos(gen)}});
+    end = std::chrono::high_resolution_clock::now();
+    init_ant_time = end - start;
+
     // On crée toutes les fourmis dans la fourmilière.
     pheromone phen(land.dimensions(), pos_food, pos_nest, alpha, beta);
 
@@ -71,23 +98,28 @@ int main(int nargs, char* argv[])
     display_t displayer( land, phen, pos_nest, pos_food, ants, win );
     // Compteur de la quantité de nourriture apportée au nid par les fourmis
     size_t food_quantity = 0;
-    std::chrono::duration<double> tot;
     int ind=0;
-    int SIZE_VEC = 2000;
-    std::vector<std::chrono::duration<double,std::ratio<1, 1000>>> duration_advance(SIZE_VEC);
-    std::vector<std::chrono::duration<double,std::ratio<1, 1000>>> duration_display(SIZE_VEC);
+    int SIZE_VEC = 7000;
+    std::vector<std::chrono::duration<double,ratio>> duration_advance(SIZE_VEC);
+    std::vector<std::chrono::duration<double,ratio>> duration_display(SIZE_VEC);
+    std::chrono::duration<double,ratio> tot;
 
     gui::event_manager manager;
     manager.on_key_event(int('q'), [] (int code) { exit(0); });
     manager.on_key_event(int('t'), [&] (int code) { std::cout << "Iteration : " << ind << std::endl
-                                                             << "Advance time : " << duration_advance[ind-1].count() << " ms" << std::endl
+                                                              << "Advance time : " << duration_advance[ind-1].count() << " ms"  << ", average : "
+                                                              << utils::avg<std::chrono::duration<double,ratio>>(duration_advance, ind).count() << " ms"<< std::endl
                                                              << "Display time : " << duration_display[ind-1].count() << " ms" << std::endl
-                                                             << "Total time : " << tot.count() << " s" << std::endl;
+                                                             << "Total time : " << tot.count() /1000 << " s" << std::endl;
+            if(!is_first_time){
+                std::chrono::duration<double,ratio> res = first_food_time - start_finding;
+                std::cout << "time to find food : "<< res.count() << " ms" << std::endl;
+            }
     });
     manager.on_display([&] { displayer.display(food_quantity); win.blit(); });
     manager.on_idle([&] () {
         auto start = std::chrono::high_resolution_clock::now();
-        advance_time(land, phen, pos_nest, pos_food, ants, food_quantity);
+        advance_time(land, phen, pos_nest, pos_food, ants, food_quantity,first_food_time,is_first_time);
         auto end = std::chrono::high_resolution_clock::now();
         if (ind> SIZE_VEC ) {
             duration_advance.push_back(end - start);
@@ -108,6 +140,7 @@ int main(int nargs, char* argv[])
         ind++;
         win.blit(); 
     });
+    start_finding = std::chrono::high_resolution_clock::now();
     manager.loop();
 
     return 0;
